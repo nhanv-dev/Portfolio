9bf7f51969649caddd1c11844b7f08fec5d2750c
Trần Thanh Nhân <66198815+nhanv-dev@users.noreply.github.com>
2025-06-11 18:11:51 +0700

    Fix: Bỏ mansory

--- src/components/Mansory/index.jsx
+++ src/components/Mansory/index.jsx
@@ -1,81 +1,15 @@
-import { useState, useEffect, useMemo, useRef } from 'react';
-import { useTransition, a } from '@react-spring/web';
 import './style.css';
 
 function Masonry({ data }) {
-    const ref = useRef();
-    const [columns, setColumns] = useState(2);
-    const [width, setWidth] = useState(0);
 
-    useEffect(() => {
-        const updateColumns = () => {
-            if (window.matchMedia('(min-width: 1500px)').matches) {
-                setColumns(2);
-            } else if (window.matchMedia('(min-width: 1000px)').matches) {
-                setColumns(2);
-            } else if (window.matchMedia('(min-width: 600px)').matches) {
-                setColumns(2);
-            } else {
-                setColumns(1); // Breakpoint for mobile devices
-            }
-        };
-
-        updateColumns();
-        window.addEventListener('resize', updateColumns);
-        return () => window.removeEventListener('resize', updateColumns);
-    }, []);
-
-    useEffect(() => {
-        const handleResize = () => {
-            if (ref.current) {
-                setWidth(ref.current.offsetWidth);
-            }
-        };
-
-        handleResize(); // Set initial width
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, [ref]);
-
-    const [heights, gridItems] = useMemo(() => {
-        let heights = new Array(columns).fill(0);
-        let gridItems = data.map((child) => {
-            const column = heights.indexOf(Math.min(...heights));
-            const x = (width / columns) * column;
-            const y = (heights[column] += child.height / 2) - child.height / 2;
-            return { ...child, x, y, width: width / columns, height: child.height / 2 };
-        });
-        return [heights, gridItems];
-    }, [columns, data, width]);
-
-    const transitions = useTransition(gridItems, {
-        keys: (item) => item.id, // Use a unique key based on the id
-        from: ({ x, y, width, height }) => ({ x, y, width, height, opacity: 0 }),
-        enter: ({ x, y, width, height }) => ({ x, y, width, height, opacity: 1 }),
-        update: ({ x, y, width, height }) => ({ x, y, width, height }),
-        leave: { height: 0, opacity: 0 },
-        config: { mass: 5, tension: 500, friction: 100 },
-        trail: 25,
-    });
-
-    // Render the grid
     return (
-        <div ref={ref} className='masonry' style={{ height: Math.max(...heights) }}>
-            {transitions((style, item) => (
-                <a.div key={item.id} style={style}>
-                    <div
-                        style={{
-                            backgroundColor: '#ffffff',  
-                            width: '100%',
-                            height: '100%',
-                            backgroundImage: `url(${item.image})`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                        }}
-                    />
-                </a.div>
-            ))
-            }
+        <div className='grid grid-cols-2 gap-5'>
+            {data.map((item, index) => (
+                <div
+                    className='bg-cover bg-top h-[300px] rounded-xl'
+                    style={{ backgroundImage: `url(${item.image})` }}
+                />
+            ))}
         </div >
     );
 }
